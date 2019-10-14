68c364d006427ff7c914e14162f53be0a1831e8e
Samuel Scherer <scheresr@mail.uc.edu>
2019-10-14 19:50:25 -0400

    Started working on new player controller & behav

---
+++ Assets/Scripts/SimplifiedPlayerBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplifiedPlayerBehavior : MonoBehaviour
+{
+    public float maxMoveSpeed, accelerationSpeed, jumpSpeed;
+
+    public void Move(float magnitude)
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.AddForce(accelerationSpeed * Vector2.right * magnitude);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxMoveSpeed);
+    }
+
+    public void Jump()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+    }
+}
